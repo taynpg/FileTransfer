bf75243756e69275c310b869319b12f1021c718f
taynpg <taynpg@163.com>
2025-05-14 11:31:10 +0800

    3rd：import third party's source files.

---
+++ test/3rdlib/main.cxx
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/*----------------------------------------------------------------------
+ *
+ * This file is a test program to verify:
+ *   1. Whether a third-party library can be successfully compiled.
+ *   2. Whether the compiled library functions correctly.
+ *
+ * Usage:
+ *    - Build this file along with the target third-party library.
+ *    - Run the executable to check basic functionality.
+ *
+ *--------------------------------------------------------------------*/
+
+int main()
+{
+    return 0;
+}
